350786f531a47f7a2985baa31d79ba168a594ff9
valitovgaziz <valitovgaziz@gmail.com>
2025-02-24 09:38:12 +0500

    Добавил файлы локализации для русского языка

--- src/i18n/en-US/index.ts
+++ src/i18n/en-US/index.ts
@@ -2,6 +2,9 @@
 // so you can safely delete all default props below
 
 export default {
+  shortLangU: 'EN',
+  shortLangL: 'en',
+  lang: 'English',
   failed: 'Action failed',
   success: 'Action was successful'
 };
---
+++ src/i18n/ru-RU/index.ts
@@ -0,0 +1,10 @@
+// This is just an example,
+// so you can safely delete all default props below
+
+export default {
+  shortLangU: 'RU',
+  shortLangL: 'ru',
+  lang: 'Русский',
+  failed: 'Ошибка',
+  success: 'Успешно'
+};
